7e30a455ce0440603e2b9eb2f39827c94b98bc27
shimaa-mahmoud <shamiaa220@gmail.com>
2019-02-04 16:21:14 +0200

    Update config.php

--- mom WEB/config.php
+++ mom WEB/config.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
-	$username = "username";
-	$password = "password";
+	$username = "root";
+	$password = "";
 	$dbname = "mom";
 
 	$db = mysqli_connect($servername, $username, $password, $dbname);
