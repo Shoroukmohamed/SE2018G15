57102095728a2a1d2560ce660867aec301f53c62
Shoroukmohamed <36273234+Shoroukmohamed@users.noreply.github.com>
2019-02-15 23:47:04 +0200

    Add files via upload

---
+++ final_web/lib/config.php
@@ -0,0 +1,15 @@
+<?php
+// MUTE NOTICES
+error_reporting(E_ALL & ~E_NOTICE);
+
+// LIB PATH
+define('PATH_LIB', __DIR__ . DIRECTORY_SEPARATOR);
+
+// DATABASE SETTINGS
+// ! CHANGE THESE TO YOUR OWN !
+define('DB_HOST', 'sql311.epizy.com');
+define('DB_NAME', 'epiz_23455142_MOM');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'epiz_23455142');
+define('DB_PASSWORD', '7QXe8FF1Jlgm');
+?>
